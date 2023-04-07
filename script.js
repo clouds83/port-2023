2c183f791c87153f431c31091b5106a8fde89eac
Claudio Godoy <clouds83@gmail.com>
2023-04-07 15:28:49 -0300

    mobile menu fix

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const buttonMenu = document.querySelector(".button-menu");
 const menu = document.querySelector(".menu");
 const closeMenu = document.querySelector(".close-menu");
+const body = document.querySelector("body");
 
 buttonMenu.addEventListener("click", () => {
   menu.classList.add("show-menu");
+  body.classList.add("block-screen");
 });
 
 menu.querySelectorAll("a").forEach((link) => {
   link.addEventListener("click", () => {
     menu.classList.remove("show-menu");
+    body.classList.remove("block-screen");
   });
 });
